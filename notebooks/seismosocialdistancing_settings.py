c21d5272f30edd8cca1e1fc5afca4759fcc98dde
lmoresi <>
2020-05-18 19:55:27 +1000

    Initial data cache setup

--- notebooks/seismosocialdistancing_settings.py
+++ notebooks/seismosocialdistancing_settings.py
@@ -8,11 +8,11 @@ from obspy import UTCDateTime
 # Set these values to control the notebook behaviour
 
 # Make sure you take at least a full week (>=7 days) before the first "ban"
-start = UTCDateTime("2020-01-15")
+start = UTCDateTime("2019-11-25")
 
 # Leaving UTCDateTime() empty means "now":
 # and this means 24 hours ago: UTCDateTime() - 24*3600
-end = UTCDateTime("2020-02-15")
+end = UTCDateTime() - 24*3600
 
 
 # This is the time it takes to be sure the data that we download is a complete record that
@@ -30,8 +30,10 @@ sitedesc = "Keysborough Secondary College, Victoria"
 
 data_provider = "IRIS"
 logo = None # 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Logo_SED_2014.png/220px-Logo_SED_2014.png'
-bans = {
-        "2020-01-28 00:00":"End of School Summer Holiday"
-        }
+bans = {"2019-12-20 00:00":"Start of School Summer Holiday",
+        "2020-01-28 00:00":"End of School Summer Holiday",
+        "2020-03-18 00:00":'No Large Gatherings', 
+        "2020-03-25 12:00":'Restaurants/Bars/Schools closed',
+        "2020-04-10 12:00":'Easter long weekend'}
 
 
